909174084e280427dd2937bb9525ce9728cdece2
MAHANANDA MAYEKAR <mayekardiksha750@gmail.com>
2024-11-20 19:44:33 +0530

    CHANGES ARE ADDED

---
+++ SRC/index.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import { PORT } from './config/serverConfig.js';
+import {
+	
+	StatusCodes,
+	
+} from 'http-status-codes';
+
+const app = express();
+app.get('/ping', (req, res) => {
+  res.status(StatusCodes.OK).json({
+    success: 'true',
+    message: 'this is ping request'
+  });
+});
+
+app.listen(PORT, () => {
+  console.log('server is listening on port', PORT);
+});
