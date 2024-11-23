bbfe5e400fa4000672fc9bf2e6a7455ed851169d
MAHANANDA MAYEKAR <mayekardiksha750@gmail.com>
2024-11-23 10:54:33 +0530

    added functionality to crud repository

--- SRC/repositories/crudRepository.js
+++ SRC/repositories/crudRepository.js
@@ -1,29 +1,24 @@
-export default function crudRepository(schema){
-    return {
-        model:schema,
-        create:async function(data){
-            const newUser=await this.model.create(data)
-            return newUser
-
-        },
-        findAll:async function(data){
-            const allDocs=await this.model.find()
-            return allDocs
-        },
-        findById:async function(id){
-            const doc=await this.model.findById(id)
-            return doc
-        },
-        deleteById:async function(id){
-            const response=await this.model.findByIdAndDelete(id)
-            return response
-
-        },
-        updateById:async function (id,data){
-            const updatedDoc=await this.model.findByIdAndUpdate(id,data,{new:true})
-            return updatedDoc
-        }
-
+export default function crudRepository(model) {
+  return {
+    create: async function (data) {
+      const newUser = await model.create(data);
+      return newUser;
+    },
+    findAll: async function () {
+      const allDocs = await model.find();
+      return allDocs;
+    },
+    findById: async function (id) {
+      const doc = await model.findById(id);
+      return doc;
+    },
+    deleteById: async function (id) {
+      const response = await model.findByIdAndDelete(id);
+      return response;
+    },
+    updateById: async function (id, data) {
+      const updatedDoc = await model.findByIdAndUpdate(id, data, { new: true });
+      return updatedDoc;
     }
-
+  };
 }
--- SRC/repositories/userRepository.js
+++ SRC/repositories/userRepository.js
@@ -1,26 +1,17 @@
-import User from '../schema/userSchema';
-import crudRepository from './crudRepository';
+import User from '../schema/userSchema.js';
+import crudRepository from './crudRepository.js';
 
-export const getUserByEmail = async (email) => {
-  try {
+const userRepository = {
+  ...crudRepository(User), //take all the key-value pairs (methods and properties) returned by the crudRepository function and include them in the userRepository object.
+
+  getUserByEmail: async function (email) {
     const user = await User.findOne({ email });
     return user;
-  } catch (error) {
-    console.log('cannot get user by email');
-  }
-};
-
-export const getUserByName = async (name) => {
-  try {
-    const user = await User.findOne({ username: name });
+  },
+  getUserByName: async function (username) {
+    const user = await User.findOne({ username }).select('-password'); //get email,avatar but exclude password
     return user;
-  } catch (error) {
-    console.log('cannot get user by username');
   }
 };
 
-const crudMethods=crudRepository(User)
-export default crudMethods
-
-
-
+export default userRepository;
