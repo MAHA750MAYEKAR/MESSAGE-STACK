d42d9619f7fe21e95575be597de736c3096b6331
MAHANANDA MAYEKAR <mayekardiksha750@gmail.com>
2024-11-23 07:57:12 +0530

    added user schema with robohash for dp

--- SRC/schema/userSchema.js
+++ SRC/schema/userSchema.js
@@ -3,13 +3,31 @@ import mongoose from "mongoose";
 const userSchema=new mongoose.Schema({
     email:{
         type:String,
-        require:[true,"email is required"],
+        required:[true,"email is required"],
         unique:[true,"this email is already present"],
         match: [/^[\w-\.]+@([\w-]+\.)+[a-zA-Z]{2,6}$/, "Please enter a valid email address"]
 
+    },
+    password:{
+        type:String,
+        required:[true,'password is required']
+    },
+    username:{
+        required:[true,'username is required'],
+        unique:[true,'Username already exist'],
+        match:[/^[a-zA-Z0-9_]{3,16}$/,"username only contain letters and digits and underscores"]
+    },
+    avatar:{
+        type:String
     }
 
 },{timestamps:true})
+
+userSchema.pre("save",function saveUser(next){
+    const user=this
+    user.avatar=`https://robohash.org/${user.username}`//sets random default dp to the user before saving it to DB
+    next()
+})
 
 
 const User=mongoose.model("User",userSchema)
