3137dffd2928ce7339535bc8f70cff2cc45e1eea
MAHANANDA MAYEKAR <mayekardiksha750@gmail.com>
2024-11-24 11:57:55 +0530

    added sign in service with token generation

--- SRC/service/userService.js
+++ SRC/service/userService.js
@@ -1,5 +1,9 @@
+import { StatusCodes } from 'http-status-codes';
 import userRepository from '../repositories/userRepository.js';
-import { createUser } from '../repositories/userRepository.js';
+import ClientErrors from '../utils/errors/clientErrors.js';
+import bcrypt from 'bcrypt'
+import genToken from '../utils/JWTtoken/genToken.js'
+
 
 export const userSignUpService = async function (data) {
   try {
@@ -29,3 +33,56 @@ export const userSignUpService = async function (data) {
     throw error;
   }
 };
+
+
+
+export const userSignInService= async (data)=>{
+  try {
+    if(!data.email){
+      throw new Error("email is invalid try again");
+      
+    }
+    const userExist= await userRepository.getUserByEmail(data.email)
+
+    if(!userExist){
+      throw new ClientErrors({
+        message:"invalid data sent by the client",
+        Explanation:"No registered user found with thid email",
+        StatusCode:StatusCodes.NOT_FOUND
+      })
+      
+
+    }
+
+    //if user is present than check the correct password
+
+    const isMatch=bcrypt.compareSync(data.password,userExist.password)
+
+    if(!isMatch){
+      throw new ClientErrors({
+        message:"Invalid password please try again",
+        Explanation:"Invalid data senr=t by the user",
+        StatusCode:StatusCodes.BAD_REQUEST
+      })
+      
+    }
+
+    return{
+      username:userExist.username,
+      avatar:userExist.avatar,
+      email:userExist.email,
+      Token:genToken({
+        id:userExist.id,
+        email:userExist.email
+      })
+    }
+    
+  } catch (error) {
+    
+    console.log("error in service layer in signin",error);
+    throw error;
+    
+    
+  }
+
+}
